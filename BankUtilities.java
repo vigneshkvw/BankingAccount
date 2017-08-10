0779c65c7621034d28c36eec8db459a68aa84a08
vigneshkvw <vignesh.vwk@gmail.com>
2017-08-10 11:38:39 +0530

    Add files via upload

---
+++ BankUtilities.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bankrefractor;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author viki
+ */
+class BankUtilities {
+	int acctype, year = 1, AccNo;
+	float intr;
+	String name = "";
+
+	Scanner in = new Scanner(System.in);
+	Random rnd = new Random();
+	float Cashdeposit = 0, balance = 0;
+}
---
+++ MyException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bankrefractor;
+
+/**
+ *
+ * @author viki
+ */
+class MyException extends Exception {
+MyException(String message) {
+		super(message);
+	}
+}

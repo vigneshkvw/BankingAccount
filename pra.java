bf2d320410e00120e935e809ec45347369f4d601
vigneshkvw <vignesh.vwk@gmail.com>
2017-09-09 12:37:14 +0530

    Add files via upload

---
+++ pra.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package frames;
+
+/**
+ *
+ * @author viki
+ */
+public class pra {
+    public static void main(String[] args){
+StringBuffer str =new StringBuffer("viki");
+StringBuffer str2=new StringBuffer("waran");
+ StringBuffer str3=str.append(str2);
+System.out.println(str3);
+    }
+}
